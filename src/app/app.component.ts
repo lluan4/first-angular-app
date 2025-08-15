feb4783302fc3ce19e7817f5c5c12156edbdb83a
Luan Lima <lluan.lima94@gmail.com>
2025-08-15 17:09:52 -0300

    Refactor code structure for improved readability and maintainability

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummies/dummy-users';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [HeaderComponent, UserComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'first-angular-app';
+  users = DUMMY_USERS;
+
+  onSelectUser(id: string) {
+    console.log('Selected user ID:', id);
+  }
 }
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,136 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {
+  @Input({ required: true }) id: string = '';
+  @Input({ required: true }) avatar: string = '';
+  @Input({ required: true }) name: string = '';
+
+  @Output() select = new EventEmitter<string>();
+
+  get imagePath(): string {
+    return `assets/users/${this.avatar}`;
+  }
+
+  onSelectUser() {
+    this.select.emit(this.id);
+  }
+}
+
+// import { Component,  Input, output } from '@angular/core';
+
+// @Component({
+//   selector: 'app-user',
+//   imports: [],
+//   templateUrl: './user.component.html',
+//   styleUrl: './user.component.css',
+// })
+// export class UserComponent {
+//   @Input({ required: true }) id: string = '';
+//   @Input({ required: true }) avatar: string = '';
+//   @Input({ required: true }) name: string = '';
+
+//   select = output<string>();
+
+//   get imagePath(): string {
+//     return `assets/users/${this.avatar}`;
+//   }
+
+//   onSelectUser() {
+//     this.select.emit(this.id);
+//   }
+// }
+
+// import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+// @Component({
+//   selector: 'app-user',
+//   imports: [],
+//   templateUrl: './user.component.html',
+//   styleUrl: './user.component.css',
+// })
+// export class UserComponent {
+//   @Input({ required: true }) id: string = '';
+//   @Input({ required: true }) avatar: string = '';
+//   @Input({ required: true }) name: string = '';
+
+//   @Output() select = new EventEmitter();
+
+//   get imagePath(): string {
+//     return `assets/users/${this.avatar}`;
+//   }
+
+//   onSelectUser() {
+//     this.select.emit(this.id);
+//   }
+// }
+
+// import { Component, computed, input, Input } from '@angular/core';
+
+// @Component({
+//   selector: 'app-user',
+//   imports: [],
+//   templateUrl: './user.component.html',
+//   styleUrl: './user.component.css',
+// })
+// export class UserComponent {
+//   avatar = input.required<string>();
+//   name = input.required<string>();
+
+//   imagePath = computed(() => {
+//     return `assets/users/${this.avatar()}`;
+//   });
+
+//   onSelectUser(): void {}
+// }
+
+// import { Component, Input } from '@angular/core';
+
+// @Component({
+//   selector: 'app-user',
+//   imports: [],
+//   templateUrl: './user.component.html',
+//   styleUrl: './user.component.css',
+// })
+// export class UserComponent {
+//   @Input({ required: true }) avatar: string = '';
+//   @Input({ required: true }) name: string = '';
+
+//   get imagePath(): string {
+//     return `assets/users/${this.avatar}`;
+//   }
+
+//   onSelectUser(): void {}
+// }
+
+// import { Component, computed, signal } from '@angular/core';
+// import { DUMMY_USERS } from '../dummies/dummy-users';
+
+// const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
+// @Component({
+//   selector: 'app-user',
+//   imports: [],
+//   templateUrl: './user.component.html',
+//   styleUrl: './user.component.css',
+// })
+// export class UserComponent {
+//   //selectedUser = DUMMY_USERS[randomIndex];
+//   selectedUser = signal(DUMMY_USERS[randomIndex]);
+//   imagePath = computed(() => `assets/users/${this.selectedUser().avatar}`);
+
+//   // get imagePath(): string {
+//   //   return `assets/users/${this.selectedUser.avatar}`;
+//   // }
+
+//   onSelectUser(): void {
+//     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+//     this.selectedUser.set(DUMMY_USERS[randomIndex]);
+//     //this.selectedUser = DUMMY_USERS[randomIndex];
+//   }
+// }
